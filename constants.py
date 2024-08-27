7892af4b257478289aeec936de93382aacb0ae82
ntexe <hqk.jallc@gmail.com>
2024-08-27 13:33:23 +0300

    User can now choose sorting key

--- constants.py
+++ constants.py
@@ -1,8 +1,13 @@
 THRESHOLD_DEFAULT = 0.5
+
 INTERVAL_DEFAULT = "edge"
 INTERVAL_CHOICES = ["none", "edge"]
+
+SKEY_DEFAULT = "lightness"
+SKEY_CHOICES = ["hue", "lightness", "saturation", "min_value", "max_value", "red", "green", "blue"]
 
 HELP_DESCRIPTION = "PixelSort is a python tool for sorting pixels in pictures."
 HELP_INPUT_FILE = "Input file."
 HELP_THRESHOLD = f"Threshold for edge detection. Default is {THRESHOLD_DEFAULT}."
 HELP_INTERVAL = f"Interval type. Available choices: {', '.join(INTERVAL_CHOICES)}. Default is {INTERVAL_DEFAULT}."
+HELP_SKEY = f"Sorting key. Available choices: {', '.join(SKEY_CHOICES)}. Default is {SKEY_DEFAULT}."
--- main.py
+++ main.py
@@ -8,11 +8,24 @@ import numpy as np
 from constants import *
 import pixel_utils
 
+skeys = {
+    "hue": pixel_utils.hue,
+    "lightness": pixel_utils.lightness,
+    "saturation": pixel_utils.saturation,
+    "min_value": pixel_utils.min_value,
+    "max_value": pixel_utils.max_value,
+    "red": pixel_utils.red,
+    "green": pixel_utils.green,
+    "blue": pixel_utils.blue,
+}
+
 class PixelSort:
     def __init__(self):
         self.input_file = None
         self.threshold = THRESHOLD_DEFAULT
         self.interval_type = INTERVAL_DEFAULT
+        self.skey_choice = SKEY_DEFAULT
+        self.skey = skeys[self.skey_choice]
 
         self.image_data = []
 
@@ -39,10 +52,15 @@ class PixelSort:
                                 default=INTERVAL_DEFAULT,
                                 dest="interval_type", help=HELP_INTERVAL,
                                 metavar="interval_type")
+        arg_parser.add_argument("-s", choices=SKEY_CHOICES,
+                                default=SKEY_DEFAULT, dest="skey_choice",
+                                help=HELP_SKEY, metavar="skey_choice")
         args = arg_parser.parse_args()
         self.input_file = args.input_file
         self.threshold = args.threshold
         self.interval_type = args.interval_type
+        self.skey_choice = args.skey_choice
+        self.skey = skeys[self.skey_choice]
 
     def sort_image(self, img):
         if self.interval_type == "edge":
@@ -50,7 +68,7 @@ class PixelSort:
 
         for y in range(img.size[1]):
             if self.interval_type == "none":
-                sorted_row = sorted([self.image_data[x,y] for x in range(img.size[0])], key=pixel_utils.lightness)
+                sorted_row = sorted([self.image_data[x,y] for x in range(img.size[0])], key=self.skey)
 
             if self.interval_type == "edge":
                 interval_begin = 0
@@ -61,9 +79,9 @@ class PixelSort:
                     if pixel_utils.lightness(edge_image[x, y]) > self.threshold:
                         if x != 0:
                             interval_end = x
-                        #print(interval_begin, interval_end, y)
+
                         if interval_end - interval_begin > 1:
-                            sorted_row[interval_begin:interval_end] = sorted(sorted_row[interval_begin:interval_end], key=pixel_utils.lightness)
+                            sorted_row[interval_begin:interval_end] = sorted(sorted_row[interval_begin:interval_end], key=self.skey)
 
                         interval_begin = x+1
 
