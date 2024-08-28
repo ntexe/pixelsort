79947370a447d60e56a7043c543bbc11e36308a2
ntexe <hqk.jallc@gmail.com>
2024-08-28 17:14:17 +0300

    Minor changes
    Improved melting segmentation, perfomance (a bit) and interval is called a segment now.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 THRESHOLD_DEFAULT = 0.5
 
-INTERVAL_DEFAULT = "edge"
-INTERVAL_CHOICES = ["none", "edge", "melting"]
+SEGMENTATION_DEFAULT = "edge"
+SEGMENTATION_CHOICES = ["none", "edge", "melting"]
 
 SKEY_DEFAULT = "lightness"
 SKEY_CHOICES = ["hue", "lightness", "saturation", "min_value", "max_value", "red", "green", "blue"]
@@ -11,6 +11,6 @@ ANGLE_DEFAULT = 0
 HELP_DESCRIPTION = "PixelSort is a python tool for sorting pixels in pictures."
 HELP_INPUT_FILE = "Input file."
 HELP_THRESHOLD = f"Threshold for edge detection. Default is {THRESHOLD_DEFAULT}."
-HELP_INTERVAL = f"Interval type. Available choices: {', '.join(INTERVAL_CHOICES)}. Default is {INTERVAL_DEFAULT}."
+HELP_SEGMENTATION = f"Segmentation. Available choices: {', '.join(SEGMENTATION_CHOICES)}. Default is {SEGMENTATION_DEFAULT}."
 HELP_SKEY = f"Sorting key. Available choices: {', '.join(SKEY_CHOICES)}. Default is {SKEY_DEFAULT}."
 HELP_ANGLE = f"Angle to rotate the image before sorting in degrees. Default is {ANGLE_DEFAULT}"
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class PixelSort:
     def __init__(self):
         self.input_file = None
         self.threshold = THRESHOLD_DEFAULT
-        self.interval_type = INTERVAL_DEFAULT
+        self.segmentation = SEGMENTATION_DEFAULT
         self.skey_choice = SKEY_DEFAULT
         self.skey = skeys[self.skey_choice]
         self.angle = ANGLE_DEFAULT
@@ -51,7 +51,7 @@ class PixelSort:
 
             print("saving...")
 
-            rimg.save(f"{os.path.basename(os.path.realpath(img.filename))}_t{self.threshold}_i_{self.interval_type}_s_{self.skey_choice}_a{self.angle}.png")
+            rimg.save(f"{os.path.basename(os.path.realpath(img.filename))}_t{self.threshold}_sg_{self.segmentation}_sk_{self.skey_choice}_a{self.angle}.png")
 
         print(f"finished in {time.monotonic()-start_time} seconds.")
 
@@ -61,11 +61,11 @@ class PixelSort:
         arg_parser.add_argument("-t", default=THRESHOLD_DEFAULT,
                                 dest="threshold", help=HELP_THRESHOLD,
                                 metavar="threshold", type=float)
-        arg_parser.add_argument("-i", choices=INTERVAL_CHOICES,
-                                default=INTERVAL_DEFAULT,
-                                dest="interval_type", help=HELP_INTERVAL,
-                                metavar="interval_type")
-        arg_parser.add_argument("-s", choices=SKEY_CHOICES,
+        arg_parser.add_argument("-sg", choices=SEGMENTATION_CHOICES,
+                                default=SEGMENTATION_DEFAULT,
+                                dest="segmentation", help=HELP_SEGMENTATION,
+                                metavar="segmentation")
+        arg_parser.add_argument("-sk", choices=SKEY_CHOICES,
                                 default=SKEY_DEFAULT, dest="skey_choice",
                                 help=HELP_SKEY, metavar="skey_choice")
         arg_parser.add_argument("-a", default=ANGLE_DEFAULT, dest="angle",
@@ -74,40 +74,40 @@ class PixelSort:
         args = arg_parser.parse_args()
         self.input_file = args.input_file
         self.threshold = args.threshold
-        self.interval_type = args.interval_type
+        self.segmentation = args.segmentation
         self.skey_choice = args.skey_choice
         self.skey = skeys[self.skey_choice]
         self.angle = args.angle
 
     def sort_image(self, img):
-        if self.interval_type == "edge":
+        if self.segmentation == "edge":
             edge_image = img.filter(ImageFilter.FIND_EDGES).load()
 
         for y in range(img.size[1]):
             print(f"\r{y}/{img.size[1]} rows", end="")
-            if self.interval_type == "none":
+            if self.segmentation == "none":
                 sorted_row = sorted([self.image_data[x,y] for x in range(img.size[0])], key=self.skey)
 
-            if self.interval_type == "edge":
-                interval_begin = 0
-                interval_end = 0
+            if self.segmentation == "edge":
+                segment_begin = 0
                 sorted_row = list([self.image_data[x,y] for x in range(img.size[0])])
 
                 for x in range(img.size[0]):
                     if pixel_utils.lightness(edge_image[x, y]) > self.threshold:
-                        interval_end = x
+                        if x - segment_begin > 1:
+                            sorted_row[segment_begin:x] = sorted(sorted_row[segment_begin:x], key=self.skey)
 
-                        if interval_end - interval_begin > 1:
-                            sorted_row[interval_begin:interval_end] = sorted(sorted_row[interval_begin:interval_end], key=self.skey)
+                        segment_begin = x+1
 
-                        interval_begin = x+1
-
-            if self.interval_type == "melting":
+            if self.segmentation == "melting":
                 sorted_row = list([self.image_data[x,y] for x in range(img.size[0])])
                 width = 100 + random.randint(0, 20)
+                offset = random.randint(0, 120)
 
-                for x in range(random.randint(0, 120), img.size[0], width):
-                    sorted_row[x:min(x+width, img.size[0])] = sorted(sorted_row[x:min(x+width, img.size[0])], key=self.skey)
+                sorted_row[0:offset] = sorted(sorted_row[0:offset], key=self.skey)
+
+                for x in range(offset, img.size[0], width):
+                    sorted_row[x:x+width] = sorted(sorted_row[x:x+width], key=self.skey)
 
             for x in range(img.size[0]):
                 self.image_data[x,y] = sorted_row[x]
