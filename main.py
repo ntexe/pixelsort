8e91c1f7058980989fc1d5fe095f4bd19c322090
ntexe <hqk.jallc@gmail.com>
2024-09-19 18:19:00 +0300

    Distribute sort functions to new file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from constants import *
 import pixel_utils
 from utils import Option, SortParams
 from options import Options, gen_options
+import sorting
 
 skeys = {
     "hue": pixel_utils.hue,
@@ -402,7 +403,7 @@ class PixelSort:
         x2 = rimg_size[0]-x1
         y2 = rimg_size[1]-y1
 
-        chunky_offset = 0
+        self.chunky_offset = 0
 
         sg = self.options.sg.value
         re = self.options.re.value
@@ -425,69 +426,19 @@ class PixelSort:
                 continue
 
             if sg == "none":
-                row.sort(key=self.skey, reverse=re)
+                sorting.none_sort(self, row, re)
 
             if sg == "edge":
-                segment_begin = 0
-
-                edge_row = self.edge_image_data[yoffset+start_x:yoffset+end_x]
-
-                for x in range(len(row)):
-                    if pixel_utils.lightness(edge_row[x]) > t*255:
-                        if x - segment_begin > 1:
-                            row[segment_begin:x] = sorted(
-                                        row[segment_begin:x], key=self.skey,
-                                        reverse=re)
-
-                        segment_begin = x+1
+                sorting.edge_sort(self, row, yoffset, start_x, end_x, t, re)
 
             if sg == "melting":
-                width = sz*self.img_size[0]*(1-(0.5*(random.random()+0.5)))
-
-                x = 0
-                while x < len(row):
-                    last_x = round(x)
-                    x += width*random.random() if x == 0 else width
-
-                    row[last_x:round(x)] = sorted(row[last_x:round(x)], key=self.skey,
-                                            reverse=re)
+                sorting.melting_sort(self, row, sz, re)
 
             if sg == "blocky":
-                block_size = sz*self.img_size[0]
-                offset = round(block_size*r*(random.random() - 0.5))
-
-                x = (start_x//block_size)*block_size
-                first_iter = True
-
-                while x < end_x:
-                    last_x = max(round(x)-start_x, 0)
-
-                    x += block_size + offset * first_iter
-                    x = max(x, start_x)
-
-                    if max(0, end_x-x) <= -offset+1:
-                        x -= offset
-
-                    row[last_x:round(x)-start_x] = sorted(row[last_x:round(x)-start_x], key=self.skey,
-                                            reverse=(y//block_size)%2 != re)
-
-                    first_iter = False
+                sorting.blocky_sort(self, row, y, start_x, end_x, sz, r, re)
 
             if sg == "chunky":
-                offset = 0
-                x = -(l-chunky_offset)
-
-                while x < len(row):
-                    last_offset = offset
-                    offset = round(l*r*(random.random() - 0.5))
-
-                    last_x = max(x, 0)
-                    x += l
-
-                    row[last_x+last_offset:x+offset] = sorted(row[last_x+last_offset:x+offset], key=self.skey,
-                                            reverse=re)
-
-                chunky_offset = (((((len(row) - chunky_offset) // l)+1) * l) + chunky_offset) % len(row)
+                sorting.chunky_sort(self, row, l, r, re)
             
             self.image_data[yoffset+start_x:yoffset+end_x] = row
 
